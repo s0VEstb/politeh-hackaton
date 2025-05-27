3ecaf70f96bac290716407a06b26d9eee618bec2
s0VEstb <attokurov090506@gmail.com>
2025-05-27 16:48:43 +0600

    gdfgdf

--- core/settings/cors.py
+++ core/settings/cors.py
@@ -4,7 +4,6 @@ from decouple import config
 
 CORS_ALLOW_ALL_ORIGINS = config('CORS_ALLOW_ALL_ORIGINS', 'False') == 'True'
 CORS_ALLOW_CREDENTIALS = config('CORS_ALLOW_CREDENTIALS', 'False') == 'True'
-CORS_ALLOW_HEADERS = config('CORS_ALLOW_HEADERS', '*')
 CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE', 'False') == 'True'
 
 
