2de33d944ec6bd2b2adca55744d3364728034cc0
s0VEstb <attokurov090506@gmail.com>
2025-05-27 17:18:52 +0600

    vdfvd

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     is_verified = models.BooleanField(default=False)
+    middle_name = models.CharField(
+        max_length=150,
+        blank=True,        # необязательное
+        verbose_name='Отчество'
+    )
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,13 +1,49 @@
 from djoser.serializers import UserCreateSerializer as BaseCreate, UserSerializer as BaseUser
+from rest_framework import serializers
 from .models import User
 
 
 class UserCreateSerializer(BaseCreate):
-    class Meta(BaseCreate.Meta):
+    # явно объявляем re_password
+    re_password = serializers.CharField(write_only=True)
+
+    class Meta:
         model = User
-        fields = ('id', 'email', 'username', 'password')
+        fields = (
+            'id',
+            'email',
+            'username',
+            'password',
+            're_password',
+            'first_name',
+            'last_name',
+            'middle_name',
+        )
+        extra_kwargs = {
+            'username':   {'required': True},
+            'email':      {'required': True},
+            'password':   {'write_only': True, 'required': True},
+            're_password':{'write_only': True, 'required': True},
+            'first_name': {'required': True},
+            'last_name':  {'required': True},
+            'middle_name':{'required': True},
+        }
+
+    def validate(self, data):
+        if data['password'] != data.pop('re_password'):
+            raise serializers.ValidationError({"re_password": "Пароли не совпадают."})
+        return data
+
 
 class UserSerializer(BaseUser):
     class Meta(BaseUser.Meta):
         model = User
-        fields = ('id', 'email', 'username', 'is_verified')
+        fields = (
+            'id',
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'middle_name',
+            'is_verified',
+        )
